a75ceb77dc6e3cd6114cf79205a9ca2281eb93fb
anshulbansal17 <your_email_address@example.com>
2021-09-29 13:55:46 +0530

    signin and register validation

--- BackEnd/controllers/register.js
+++ BackEnd/controllers/register.js
@@ -1,5 +1,8 @@
 const handleRegister = (req, res, postgres, bcrypt) => {
   const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json("Bad Request, Incorrect form Submission!");
+  }
   const hash = bcrypt.hashSync(password);
   console.log("email is", email);
   postgres
--- BackEnd/controllers/signin.js
+++ BackEnd/controllers/signin.js
@@ -1,15 +1,19 @@
 const handleSignIn = (req, res, postgres, bcrypt) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json("Bad Request, Incorrect form Submission!");
+  }
   postgres
     .select("email", "hash")
-    .where("email", "=", req.body.email)
+    .where("email", "=", email)
     .from("login")
     .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return postgres
           .select("*")
           .from("users")
-          .where("email", "=", req.body.email)
+          .where("email", "=", email)
           .then((user) => {
             res.json(user[0]);
           })
@@ -17,7 +21,7 @@ const handleSignIn = (req, res, postgres, bcrypt) => {
       } else res.status(400).json("wrong credentials");
     })
     .catch((err) => res.status(400).json("wrong credentials"));
-}
+};
 
 module.exports = {
   handleSignIn: handleSignIn,
